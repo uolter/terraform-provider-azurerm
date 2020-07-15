9a2570c1155188c1b6bfe8fd7664f6a770ea5578
tombuildsstuff <git@ibuildstuff.co.uk>
2020-07-15 07:20:21 +0200

    docs

--- azurerm/internal/services/example/resource_group_data_test.go
+++ azurerm/internal/services/example/resource_group_data_test.go
@@ -6,8 +6,10 @@ import (
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/common"
 )
 
+// check complies with the interface
 var _ common.DataSource = ResourceGroupDataSource{}
 
+// check the implementation (fields) are valid
 func TestValidateDataSource(t *testing.T) {
 	common.ValidateDataSource(t, ResourceGroupDataSource{})
 }
