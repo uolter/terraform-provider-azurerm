ceb2979d5f64dfd35a9a38d99eb44bf4a0ec5112
kt <kt@katbyte.me>
2020-11-18 18:58:40 -0800

    client config

--- azurerm/internal/services/authorization/client_config_data_source_test.go
+++ azurerm/internal/services/authorization/client_config_data_source_test.go
@@ -6,41 +6,27 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/acceptance/check"
 )
 
 func TestAccDataSourceAzureRMClientConfig_basic(t *testing.T) {
 	data := acceptance.BuildTestData(t, "data.azurerm_client_config", "current")
+	subscriptionId := os.Getenv("ARM_SUBSCRIPTION_ID")
 	clientId := os.Getenv("ARM_CLIENT_ID")
 	tenantId := os.Getenv("ARM_TENANT_ID")
-	subscriptionId := os.Getenv("ARM_SUBSCRIPTION_ID")
 
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { acceptance.PreCheck(t) },
-		Providers: acceptance.SupportedProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCheckArmClientConfig_basic,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(data.ResourceName, "client_id", clientId),
-					resource.TestCheckResourceAttr(data.ResourceName, "tenant_id", tenantId),
-					resource.TestCheckResourceAttr(data.ResourceName, "subscription_id", subscriptionId),
-					testAzureRMClientConfigGUIDAttr(data.ResourceName, "object_id"),
-				),
-			},
+	data.DataSourceTest(t, []resource.TestStep{
+		{
+			Config: `
+data "azurerm_client_config" "current" { }
+`,
+			Check: resource.ComposeTestCheckFunc(
+				check.That(data.ResourceName).Key("client_id").HasValue(clientId),
+				check.That(data.ResourceName).Key("tenant_id").HasValue(tenantId),
+				check.That(data.ResourceName).Key("subscription_id").HasValue(subscriptionId),
+				resource.TestMatchResourceAttr(data.ResourceName, "object_id", regexp.MustCompile("^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$")),
+			),
 		},
 	})
 }
-
-func testAzureRMClientConfigGUIDAttr(name, key string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		r := regexp.MustCompile("^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$")
-
-		return resource.TestMatchResourceAttr(name, key, r)(s)
-	}
-}
-
-const testAccCheckArmClientConfig_basic = `
-data "azurerm_client_config" "current" { }
-`
